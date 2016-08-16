513aa881f9ae837a2994d44da9f1125cc9f489e5
NNamdi Williams <nnamdiwill@gmail.com>
2016-08-15 22:00:08 -0400

    fizzbuzz refactor basic structure

---
+++ fizzbuzz.js
@@ -0,0 +1,9 @@
+for (var i = 1; i < 100; i++) {
+    if (i % 3 == 0) {
+        document.write("fizz");
+    } else if (i % 5 == 0) {
+        document.write("buzz");
+    } else {
+        document.write(i);
+    }
+}
